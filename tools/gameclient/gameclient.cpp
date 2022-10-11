cbbab7437577bfcd51beb734d141f3fa2944825c
Adham Sorour <asorour@sfu.ca>
2022-10-11 11:17:15 -0700

    initial project structure - compiles and runs using cmake

---
+++ tools/gameclient/gameclient.cpp
@@ -0,0 +1,11 @@
+#include "client.h"
+#include "game.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+  std::cout << "Welcome to Social Gaming!\n";
+
+  return 0;
+}
---
+++ tools/gameserver/gameserver.cpp
@@ -0,0 +1,13 @@
+#include "server.h"
+#include "game.h"
+#include "interpreter.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+  std::cout << "Game Server Initiated\n";
+
+
+  return 0;
+}
