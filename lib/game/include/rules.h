5601e09875e9b2be7acf84e4318eb485a1c38814
Ash Peng <ash@Carb.localdomain>
2022-10-17 20:41:04 -0700

    Added declarations & stubs for list and rules

--- lib/game/include/rules.h
+++ lib/game/include/rules.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "list.h"
+
+#include <vector>
+
+// Rule Interface //
+
+class Rule {
+public:
+    virtual ~Rule();
+    virtual void execute() const = 0;
+};
+
+
+// Control Structures//
+
+class Foreach : public Rule {
+    const List list;
+    const std::vector<Rule> rules;
+public:
+    /*Foreach(List list, std::vector<Rule> rules);*/
+    void execute();
+};
--- lib/game/src/rules.cpp
+++ lib/game/src/rules.cpp
@@ -1,1 +1,8 @@
 #include "rules.h"
+
+/*Foreach::Foreach(List _list, std::vector<Rule> _rules) 
+    : list(_list), rules(_rules) {}
+
+void Foreach::execute() {
+    
+}*/
