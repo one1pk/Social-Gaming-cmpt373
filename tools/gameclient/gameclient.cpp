7c8ce959d16d62fe142e84f9d8b86e67f8476c53
Ash Peng <ash@Carb.localdomain>
2022-11-20 14:12:46 -0800

    Updated initialization to be only in game client

--- tools/gameclient/gameclient.cpp
+++ tools/gameclient/gameclient.cpp
@@ -26,7 +26,7 @@ std::string WELCOME_MESSAGE = (
         "  * To display help information again enter: help\n\n");
 
 int main(int argc, char* argv[]) {
-    google::InitGoogleLogging(argv[0]);
+    // google::InitGoogleLogging(argv[0]);
     
     // verify that the cmdline arguments provided describe a game server
     // and connect to the server
