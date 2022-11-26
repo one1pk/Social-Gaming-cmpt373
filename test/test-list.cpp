95d05268f0a4ba044e5c231804ac294f4ff3f2b7
Ash Peng <rpa47@sfu.ca>
2022-11-25 16:35:32 -0800

    Fixed getVectorTest and pushing draft of getSubListTest

--- test/test-list.cpp
+++ test/test-list.cpp
@@ -8,6 +8,10 @@ using namespace std;
 using namespace testing;
 using ::testing::AtLeast;
 
+/**
+Map elements
+*/
+
 TEST (ElementTest, getMapElementTest) {
     ElementSptr test_element_string = make_shared<Element<std::string>>("This is a test");
     map<string, shared_ptr<ListElement>> map_sample {{"first", test_element_string}};
@@ -65,9 +69,28 @@ TEST(ElementTest, getVectorTest) {
 
     ElementVector test_vector = temp_vector;
     ElementSptr test_vector_ptr = make_shared<Element<ElementVector>>(test_vector);
-    EXPECT_EQ(
-        test_vector_ptr->getVector(), 
-        // TODO: What to compare?
-        // vector({make_shared<int>(1), make_shared<int>(2), make_shared<int>(3)})
-    );
-}
+    EXPECT_EQ(test_vector_ptr->getVector(), test_vector);}
+
+
+// // TODO: The following test is failing
+
+// TEST(ElementTest, getSubListTest) {
+//     ElementVector vector_of_maps;
+
+//     ElementSptr first_string = make_shared<Element<std::string>>("First string");
+//     map<string, shared_ptr<ListElement>> first_map {{"test", first_string}};
+//     auto first_map_ptr = make_shared<map<string, shared_ptr<ListElement>>>(first_map);
+    
+//     ElementSptr second_string = make_shared<Element<std::string>>("Second string");
+//     map<string, shared_ptr<ListElement>> second_map {{"test", second_string}};
+//     auto second_map_ptr = make_shared<map<string, shared_ptr<ListElement>>>(second_map);
+
+//     vector_of_maps.push_back(first_map_ptr);
+//     vector_of_maps.push_back(second_map_ptr);
+
+//     ElementVector res_vector;
+//     res_vector.push_back(first_string);
+//     res_vector.push_back(second_string);
+
+//     EXPECT_EQ(vector_of_maps.getSubList("test"), res_vector);
+// }
